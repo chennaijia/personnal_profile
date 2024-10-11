4c81e2d3aa566b360c6f4b41e0791ca264ac0577
chennaijia <naijia030@gmail.com>
2024-10-11 21:14:31 +0800

    first commit

---
+++ app.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="bg-white py-10 flex flex-col items-center">
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <div class="flex justify-around flex-row item-center">
+            <slot />
+        </div>
+    </div>
+</template>
